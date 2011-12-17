6fd37e9bc07fd7cc649622e5a5bf3fd0e296d1cd
Wodor Wodorski <wodor@wodor.net>
2011-12-17 18:23:49 +0100

    preparing

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,7 @@
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
-    '::1',
+    '::1','192.168.1.108'
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
