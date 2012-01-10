698df5d97fa0f56f67401739b1643c4574f89c6f
Wodor Wodorski <wodor@wodor.net>
2012-01-10 01:02:02 +0100

    roadTypes : intl extension for twig, new look for trip  - start

--- src/WodorNet/MotoTripBundle/Twig/Extension/MotoTripExtension.php
+++ src/WodorNet/MotoTripBundle/Twig/Extension/MotoTripExtension.php
@@ -10,7 +10,8 @@ use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 class MotoTripExtension extends \Twig_Extension
 {
 
-    public function getName() {
+    public function getName()
+    {
         return 'mototrip';
     }
 
@@ -18,31 +19,48 @@ class MotoTripExtension extends \Twig_Extension
     {
         return array(
             'crop' => new \Twig_Filter_Method($this, 'crop'),
+            'intldate' => new \Twig_Filter_Method($this, 'intldate'),
         );
     }
 
 
-    function crop($str, $len) {
-        if ( strlen($str) <= $len ) {
+    public function intldate(\DateTime $str, $format = "eeee d MMMM yyyy")
+    {
+
+        $formatter = new \IntlDateFormatter(
+            \Locale::getDefault(),
+            \IntlDateFormatter::MEDIUM,
+            \IntlDateFormatter::SHORT,
+            null,
+            null,
+            $format
+        );
+
+        return $formatter->format($str);
+    }
+
+    public function crop($str, $len)
+    {
+        if (strlen($str) <= $len) {
             return $str;
         }
 
         // find the longest possible match
         $pos = 0;
-        foreach ( array('. ', '? ', '! ') as $punct ) {
+        foreach (array('. ', '? ', '! ') as $punct) {
             $npos = strpos($str, $punct);
-            if ( $npos > $pos && $npos < $len ) {
+            if ($npos > $pos && $npos < $len) {
                 $pos = $npos;
             }
         }
 
-        if ( !$pos ) {
+        if (!$pos) {
             // substr $len-3, because the ellipsis adds 3 chars
-            return substr($str, 0, $len-3) . '...';
+            return substr($str, 0, $len - 3) . '...';
         }
         else {
             // $pos+1 to grab punctuation mark
-            return substr($str, 0, $pos+1);
+            return substr($str, 0, $pos + 1);
         }
     }
 
